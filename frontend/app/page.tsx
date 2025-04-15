75aa01777a3fee259ed4d868a874858b0636833f
Waleed Raza <waleedraza1211@gmail.com>
2025-04-14 20:27:22 -0400

    initial commit: setup next.js with tailwind

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-blue-900 text-white">
+      <h1 className="text-4xl font-bold">Tailwind is working!</h1>
+    </main>
+  );
+}
